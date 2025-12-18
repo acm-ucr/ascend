79986bc723f790910a71a4f59acc9b47814f887b
unknown <twu148@ucr.edu>
2025-12-17 18:02:44 -0800

    feat: edit to p tag structure

--- src/components/home/AboutUs.tsx
+++ src/components/home/AboutUs.tsx
@@ -6,22 +6,23 @@ import Heading from "../Heading";
 
 const AboutUs = () => {
   return (
-    <div className="font-nunito mx-auto flex w-4/5 flex-col gap-8 p-20 text-2xl/11 max-lg:w-full max-lg:p-16 max-lg:text-xl/8">
+    <div className="font-nunito m-8 mx-auto mt-16 flex w-4/5 flex-col gap-8 text-lg md:text-2xl">
       <Heading title="About Us" />
-
-      <p className="mt-10">
-        Ascend UCR is a one of the student-led chapters of Ascend Leadership,
-        the largest non-profit membership organizations advancing Pan-Asian
-        professionals in the workplace. Our objective is to empower Asian and
-        Pacific Islander (API) business leaders of tomorrow to build community
-        and ignite change.
-      </p>
-      <p className="mb-5">
-        Through our UCR student chapter we host weekly events such as
-        professional workshops, guest speakers, social events, and more to
-        promote personal/professional development and create lifelong
-        friendships.
-      </p>
+      <div className="mx-8 my-4 flex flex-col gap-8 text-center md:my-12 md:text-left md:indent-12">
+        <p>
+          Ascend UCR is a one of the student-led chapters of Ascend Leadership,
+          the largest non-profit membership organizations advancing Pan-Asian
+          professionals in the workplace. Our objective is to empower Asian and
+          Pacific Islander (API) business leaders of tomorrow to build community
+          and ignite change.
+        </p>
+        <p>
+          Through our UCR student chapter we host weekly events such as
+          professional workshops, guest speakers, social events, and more to
+          promote personal/professional development and create lifelong
+          friendships.
+        </p>
+      </div>
 
       <div className="col-span-3 flex flex-row justify-between text-4xl max-lg:flex-col max-lg:items-center max-lg:gap-6 max-lg:text-2xl">
         <div className="flex flex-col items-center gap-4">
