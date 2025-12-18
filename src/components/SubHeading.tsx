a2e5a857ed1d0a1eac2e3c7475d2f174d71bf44c
unknown <twu148@ucr.edu>
2025-12-17 18:22:37 -0800

    feat: Join us edits

--- src/components/SubHeading.tsx
+++ src/components/SubHeading.tsx
@@ -6,7 +6,7 @@ const SubHeading = ({ title }: SubHeadingProps) => {
   return (
     <div className="mx-auto w-3/4 text-center">
       <div className="border-t border-black" />
-      <p className="text-ascend-red-orange font-playfair my-8 text-3xl font-bold md:text-4xl">
+      <p className="text-ascend-red-orange font-playfair mt-12 mb-8 text-4xl font-bold md:text-5xl">
         {title}
       </p>
       <div className="border-t border-black" />
